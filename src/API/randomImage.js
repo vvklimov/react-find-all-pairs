809ec76dcf12107f0eaf3bb998939b348987046a
youllneverreachme <elmpelmom@gmail.com>
2024-02-13 08:00:22 +0100

    fixed wrong data caching for surprise me theme on mobile devices

--- src/API/randomImage.js
+++ src/API/randomImage.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { decks } from "../utils/data";
-const url = "https://picsum.photos/159/250.webp";
+const url = "https://picsum.photos/159/250.webp?random=";
 
 const RandomImageQuery = (shuffledArray, currentTheme) => {
   return {
@@ -8,7 +8,9 @@ const RandomImageQuery = (shuffledArray, currentTheme) => {
     enabled: currentTheme === "surprise-me" && shuffledArray.length !== 0,
     queryFn: async () => {
       const requiredNumOfQueries = [...new Set(shuffledArray)];
-      const respPromises = requiredNumOfQueries.map(() => axios.get(url));
+      const respPromises = requiredNumOfQueries.map((_, index) =>
+        axios.get(`${url}${index}`)
+      );
       const result = await Promise.all(respPromises);
       const newDeck = result.map((item) => {
         return { cardSrc: item?.request?.responseURL };
