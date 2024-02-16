b59471f20b69386dd8a397f43736a9a05254e4d6
youllneverreachme <elmpelmom@gmail.com>
2024-02-16 09:31:41 +0100

    Revert "updated loading screen to force user interact with the page before any action with an audio is happened"
    This reverts commit fbde7eb7ef1227c9f521e92044379fcc6cec8436.

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { setIsLoaded } from "../features /transfers/transfersSlice";
 import preloadImages from "image-preload";
-import SettingsBtn from "./SettingsBtn";
 const Loading = () => {
   const dispatch = useDispatch();
   const { "show-rules": showRules } = useSelector((state) => {
@@ -10,14 +9,13 @@ const Loading = () => {
       "show-rules": state.settings.settings.other["show-rules"],
     };
   }, shallowEqual);
-  const [showBtn, setShowBtn] = useState(false);
   useEffect(() => {
     const handleLoading = () => {
       const images = document.querySelectorAll("img");
       const imageURLs = Array.from(images).map((img) => img.src);
       preloadImages(imageURLs, {
         onComplete: () => {
-          setShowBtn(true);
+          dispatch(setIsLoaded(true));
         },
       });
     };
@@ -30,37 +28,17 @@ const Loading = () => {
       return window.removeEventListener("load", handleLoading);
     };
   }, []);
-  //
+
   return (
     <div className="page-loading center-items">
-      {!showBtn && (
-        <div className="loading-spinner-wrapper">
-          <div className="loading-spinner"></div>
-          <h1 className="tag-btn-gradient gradient-hover-effect">Loading...</h1>
-        </div>
-      )}
-
+      <div className="loading-spinner-wrapper">
+        <div className="loading-spinner"></div>
+        <h1 className="tag-btn-gradient gradient-hover-effect">Loading...</h1>
+      </div>
       <article className={`rules ${showRules ? "rules-show" : ""}`}>
         <h3>rules are simple:</h3>
         <p>find all card pairs before time is up</p>
       </article>
-      {showBtn && (
-        <>
-          <div className="settings-container">
-            <div className="single-setting center-items">
-              <SettingsBtn tag={"other"} subtagClass={"sound-effects"} />
-              <span>{"sound effects"}</span>
-            </div>
-            <div className="single-setting center-items">
-              <SettingsBtn tag={"other"} subtagClass={"show-rules"} />
-              <span>{"show rules"}</span>
-            </div>
-          </div>
-          <button className="btn" onClick={() => dispatch(setIsLoaded(true))}>
-            start new game
-          </button>
-        </>
-      )}
     </div>
   );
 };
