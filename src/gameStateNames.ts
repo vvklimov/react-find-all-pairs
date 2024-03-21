c47b289a96567d7ff5d186b26afae09351743929
youllneverreachme <elmpelmom@gmail.com>
2024-03-21 14:20:20 +0100

    update: gameStateNames changed

---
+++ src/gameStateNames.ts
@@ -0,0 +1,8 @@
+import { GameStateName } from "./utils/types";
+
+export const IDLE: GameStateName = "IDLE";
+export const PAUSE: GameStateName = "PAUSE";
+export const GAMEOVER_FAILURE: GameStateName = "GAMEOVER_FAILURE";
+export const GAMEOVER_SUCCESS: GameStateName = "GAMEOVER_SUCCESS";
+export const GAME: GameStateName = "GAME";
+export const RESUME: GameStateName = "RESUME";
