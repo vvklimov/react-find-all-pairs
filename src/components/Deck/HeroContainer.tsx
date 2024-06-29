cddf89155796b4b1b904628ab628cb56cca31a7d
youllneverreachme <elmpelmom@gmail.com>
2024-06-30 00:40:25 +0200

    update HeroContainer

--- src/components/Deck/HeroContainer.tsx
+++ src/components/Deck/HeroContainer.tsx
@@ -1,19 +1,24 @@
 import { useRef } from "react";
-import { shallowEqual, useSelector } from "react-redux";
+import { shallowEqual } from "react-redux";
 import { useQuery } from "@tanstack/react-query";
 import { RandomImageQuery } from "../../API/randomImage";
 import { RandomPersonQuery } from "../../API/randomPerson";
 import DeckContainer from "./DeckContainer";
 import useHandleResize from "./hooks/useHandleResize";
+import { useAppSelector } from "../../utils/hooks";
+
 const HeroContainer = () => {
-  const heroRef = useRef();
-  const { currentSize, currentTheme, shuffledArray } = useSelector((state) => {
-    return {
-      currentSize: state.settings.currentSize,
-      currentTheme: state.settings.settings.themes,
-      shuffledArray: state.deck.shuffledArray,
-    };
-  }, shallowEqual);
+  const heroRef = useRef<HTMLDivElement | null>(null);
+  const { currentSize, currentTheme, shuffledArray } = useAppSelector(
+    (state) => {
+      return {
+        currentSize: state.settings.currentSize,
+        currentTheme: state.settings.settings.themes,
+        shuffledArray: state.deck.shuffledArray,
+      };
+    },
+    shallowEqual
+  );
 
   useQuery(RandomImageQuery(shuffledArray, currentTheme));
   useQuery(RandomPersonQuery(shuffledArray, currentTheme));
