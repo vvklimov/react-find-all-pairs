86613533c2f3ba3e6fdc5f4f30bf375df98214fc
youllneverreachme <elmpelmom@gmail.com>
2024-06-30 00:39:26 +0200

    update useHandleCardsTransitions

--- src/components/Deck/hooks/useHandleCardsTransitions.ts
+++ src/components/Deck/hooks/useHandleCardsTransitions.ts
@@ -1,26 +1,32 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import {
   setCardsAspectRatio,
   setCardsTransitions,
 } from "../../../features/transfers/transfersSlice";
 import { getContainerData } from "../../../utils/helpers";
-const useHandleCardsTransitions = (cardsRef, currentSize, heroCenter) => {
-  const dispatch = useDispatch();
+import { HeroCenter } from "../../../utils/types";
+import { useAppDispatch } from "../../../utils/hooks";
+
+const useHandleCardsTransitions = (
+  cardsRef: React.MutableRefObject<HTMLDivElement[]>,
+  currentSize: number,
+  heroCenter: HeroCenter
+) => {
+  const dispatch = useAppDispatch();
   useEffect(() => {
     if (cardsRef.current?.length !== 0) {
       try {
         const handleCardsTransitions = () => {
           const cardsDataArray = cardsRef.current
             .filter((item) => item)
-            .map((wrapperRef, index) =>
-              getContainerData(wrapperRef, index, currentSize)
-            );
+            .map((wrapperRef, index) => getContainerData(wrapperRef, index));
           dispatch(setCardsTransitions(cardsDataArray));
           dispatch(setCardsAspectRatio(cardsDataArray));
         };
         handleCardsTransitions();
-      } catch (error) {}
+      } catch (error) {
+        console.log(`error in useHandleCardsTransitions: ${error}`);
+      }
     }
   }, [cardsRef.current.length, currentSize, heroCenter]);
 };
